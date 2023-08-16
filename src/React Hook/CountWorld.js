7726ce4db55a5d2eedb465d94dcf65c513069773
Nilu patel <nilusingh945164@gmail.com>
2023-08-16 17:04:39 +0530

    react

---
+++ src/React Hook/CountWorld.js
@@ -0,0 +1,25 @@
+//use of useref--The useRef Hook allows you to persist values between renders.
+import { useState, useEffect, useRef } from "react";
+
+
+function CountWorld() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
+}
+
+export default CountWorld;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+//import CountWorld from './React Hook/CountWorld';
 //import Timer from './React Hook/Timer';
-import Counter from './React Hook/Counter';
+//import Counter from './React Hook/Counter';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* use of hook **********/}
     {/* <Timer/> */}
-    <Counter/>
+    {/* <Counter/> */}
+      {/* <CountWorld/> */}
+      
+
     
 
   </React.StrictMode>
