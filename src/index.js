d1eb8b8bbdfef40ba9fac9d7030a1905c1c3d0f4
Nilu patel <nilusingh945164@gmail.com>
2024-02-03 14:33:29 +0530

    p

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//import CountWorld from './React Hook/CountWorld';
-//import Timer from './React Hook/Timer';
-//import Counter from './React Hook/Counter';
+import CountWorld from './React Hook/CountWorld';
+import Timer from './React Hook/Timer';
+import Counter from './React Hook/Counter';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,9 +11,9 @@ root.render(
   <React.StrictMode>
     <App />
     {/* use of hook **********/}
-    {/* <Timer/> */}
-    {/* <Counter/> */}
-      {/* <CountWorld/> */}
+    <Timer/>
+    <Counter/>
+      <CountWorld/>
       
 
     
